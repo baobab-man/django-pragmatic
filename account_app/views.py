1f552f71f4650f5c1758ac5f586050f79a06841e
Romeo <baobab_man@kakao.com>
2021-09-12 20:28:41 +0900

    009 post, DB save

--- account_app/views.py
+++ account_app/views.py
@@ -2,12 +2,20 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from account_app.models import Romeo
 
 
 def romeo(request):
 
     if request.method == "POST":
-        return render(request, 'account_app/romeo.html', context={'text': 'POST METHOD!!!'})
+
+        temp = request.POST.get('romeo_input')
+
+        new_romeo = Romeo()
+        new_romeo.text = temp
+        new_romeo.save()
+
+        return render(request, 'account_app/romeo.html', context={'romeo_output': new_romeo})
     else:
         return render(request, 'account_app/romeo.html',
                       context={'text': 'GET METHOD!!!'})
