f690cc159b87e3fa022d1723b688231bf687d9a1
Romeo <baobab_man@kakao.com>
2021-09-13 23:50:58 +0900

    011 sign-up

--- account_app/urls.py
+++ account_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from account_app.views import romeo
+from account_app.views import romeo, AccountCreateView
 
 app_name = "account_app"
 
 urlpatterns = [
-    path('romeo/', romeo, name='romeo')
+    path('romeo/', romeo, name='romeo'),
+    path('create/', AccountCreateView.as_view(), name='create'),
 ]
